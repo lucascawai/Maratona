364c214f021ac0d6c77d8ddd1193112c8f491f0c
Lucas Cawai <lucascawai@gmail.com>
2015-03-04 18:03:59 -0300

    Create a repository for a 'IC'

---
+++ IC/dictionary.cs
@@ -0,0 +1,28 @@
+/** Programa auxiliar C#
+    Função dictionary
+ **/
+
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+
+public class PortToEngStringDictionary {
+  public class void Main() {
+    StringDictionary portToEngDictionary = new StringDictionary();
+
+    portToEngDictionary.Add( "Alternativas", "Alternates" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    //portToEngDictionary.Add( "octavio", "lindo" );
+    portToEngDictionary.Add( "Selfie", "Selfie" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+    portToEngDictionary.Add( "Criterios", "Criteria" );
+  }
+}
